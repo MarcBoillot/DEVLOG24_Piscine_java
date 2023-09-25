7dc630cbf58778d831fac8d1cc17b0ecd273336e
C-luigi <luigi.chiappa@le-campus-numerique.fr>
2023-09-25 09:12:37 +0200

    first commit

---
+++ projetPiscine/src/main/java/fr/cnalps/projetPiscine/ProjetPiscineApplication.java
@@ -0,0 +1,13 @@
+package fr.cnalps.projetPiscine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetPiscineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetPiscineApplication.class, args);
+	}
+
+}
---
+++ projetPiscine/src/test/java/fr/cnalps/projetPiscine/ProjetPiscineApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.cnalps.projetPiscine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetPiscineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
