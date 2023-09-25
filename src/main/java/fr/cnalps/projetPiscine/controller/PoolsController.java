005e8882b33e947c8654843c07705f801c313490
Alexandre-Hilaire <alexandre.hilaire@le-campus-numerique.fr>
2023-09-25 13:59:42 +0200

    #21, #22, #23, mise à jour du controlleur avec ajout de la fonction update et delete, les différentes classes sont donc connectées par des méthodes qui s'appellent

--- src/main/java/fr/cnalps/projetPiscine/controller/PoolsController.java
+++ src/main/java/fr/cnalps/projetPiscine/controller/PoolsController.java
@@ -5,6 +5,7 @@ import fr.cnalps.projetPiscine.service.PoolsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
 import java.util.Optional;
 
 @RestController
@@ -21,5 +22,30 @@ public class PoolsController {
     public Iterable<Pools>getPools(){
         return poolsService.getPools();
     }
+    @PutMapping("/{id}")
+    public Pools updatePools(@PathVariable("id") final int id, @RequestBody Pools pools){
+        Optional<Pools> pool = poolsService.getPoolsById(id);
+        if (pool.isPresent()){
+            Pools currentPool = pool.get();
+            String name = pools.getName();
+            if (name != null) {
+                currentPool.setName(name);
+            }
+            String town = pools.getTown();
+            if (town != null){
+                currentPool.setTown(town);
+            }
+            Date date = pools.getDate();
+            if (date != null){
+                currentPool.setDate(date);
+            }
+            return currentPool;
+        }
+        else {
+            return null;
+        }
+    }
 
+    @DeleteMapping
+    public void deletePool(@PathVariable("id") final int id) {poolsService.deletePool(id);}
 }
