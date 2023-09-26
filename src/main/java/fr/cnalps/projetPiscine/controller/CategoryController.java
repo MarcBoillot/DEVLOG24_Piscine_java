a9b8613b32c79c66266b5c4f381886b9259a043a
C-luigi <luigi.chiappa@le-campus-numerique.fr>
2023-09-26 15:01:52 +0200

    javadoc for category #47

--- src/main/java/fr/cnalps/projetPiscine/controller/CategoryController.java
+++ src/main/java/fr/cnalps/projetPiscine/controller/CategoryController.java
@@ -1,43 +1,80 @@
 package fr.cnalps.projetPiscine.controller;
 
 import fr.cnalps.projetPiscine.model.Category;
-import fr.cnalps.projetPiscine.model.Observer;
 import fr.cnalps.projetPiscine.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+/**
+ * CategoryController manages the web requests related to Category entities.
+ * Provides endpoints for retrieving all Category's and getting a specific Category by ID.
+ */
 @RestController
 @RequestMapping(path="/category")
 public class CategoryController {
     @Autowired
     private final CategoryService service;
 
+    /**
+     * Constructor for CategoryController.
+     *
+     * @param service The CategoryService to handle business logic related to Category entities.
+     */
     public CategoryController(CategoryService service) {
         this.service = service;
     }
 
+    /**
+     * Creates a new Category object.
+     *
+     * @param category The Category object to be created.
+     * @return The created Category object.
+     */
     @PostMapping
     public Category createcategory(@RequestBody Category category) {
         return service.createCategory(category);
     }
 
+    /**
+     * Retrieves all Category object from the database.
+     *
+     * @return List of all Category object.
+     */
     @GetMapping
     public List<Category> getAllObserver(){
         return this.service.getAllCategory();
     }
 
+    /**
+     * Retrieves a specific Category by its ID.
+     *
+     * @param id The ID of the Category object.
+     * @return The Category entity matching the provided ID.
+     */
     @GetMapping("/{id}")
     public Category getObserverById (@PathVariable(name = "id") int id) {
         return this.service.getCategoryById(id);
     }
 
+    /**
+     * Updates a specific Category object by its ID.
+     *
+     * @param id The ID of the Category object.
+     * @param category The updated Category object.
+     * @return The updated Category object.
+     */
     @PutMapping("/{id}")
     public Category updateObserver(@PathVariable int id, @RequestBody Category category) {
         return service.updateCategory(id, category);
     }
 
+    /**
+     * Deletes a specific Category object by its ID.
+     *
+     * @param id The ID of the Category object.
+     */
     @DeleteMapping("/{id}")
     public void deleteObserver(@PathVariable int id) {
         service.deleteObserver(id);
--- src/main/java/fr/cnalps/projetPiscine/model/Category.java
+++ src/main/java/fr/cnalps/projetPiscine/model/Category.java
@@ -4,19 +4,36 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+/**
+ * Represents a category with a unique title and a description.
+ * The category's identifier is auto-generated.
+ */
+
 @Getter
 @Setter
 @Entity
 public class Category {
 
+    /**
+     * The unique identifier for the category.
+     */
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
+    /**
+     * The unique title for the category.
+     * This field cannot be null and has a maximum length of 50 characters.
+     */
+
     @Column(unique = true, nullable = false, length = 50)
     private String title;
 
-    @Column(nullable = false)
+    /**
+     * A brief description of the category.
+     */
+
     private String description;
 
 }
