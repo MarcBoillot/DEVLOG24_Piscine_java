3a4ac1690dd8b29f629731a17309d25650d0ea89
C-luigi <luigi.chiappa@le-campus-numerique.fr>
2023-09-26 14:33:00 +0200

    Read category #44

--- src/main/java/fr/cnalps/projetPiscine/controller/CategoryController.java
+++ src/main/java/fr/cnalps/projetPiscine/controller/CategoryController.java
@@ -1,12 +1,12 @@
 package fr.cnalps.projetPiscine.controller;
 
 import fr.cnalps.projetPiscine.model.Category;
+import fr.cnalps.projetPiscine.model.Observer;
 import fr.cnalps.projetPiscine.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(path="/category")
@@ -21,5 +21,15 @@ public class CategoryController {
     @PostMapping
     public Category createcategory(@RequestBody Category category) {
         return service.createCategory(category);
+    }
+
+    @GetMapping
+    public List<Category> getAllObserver(){
+        return this.service.getAllCategory();
+    }
+
+    @GetMapping("/{id}")
+    public Category getObserverById (@PathVariable(name = "id") int id) {
+        return this.service.getCategoryById(id);
     }
 }
--- src/main/java/fr/cnalps/projetPiscine/service/CategoryService.java
+++ src/main/java/fr/cnalps/projetPiscine/service/CategoryService.java
@@ -1,10 +1,13 @@
 package fr.cnalps.projetPiscine.service;
 
 import fr.cnalps.projetPiscine.model.Category;
+import fr.cnalps.projetPiscine.model.Observer;
 import fr.cnalps.projetPiscine.repository.CategoryRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CategoryService {
@@ -17,5 +20,13 @@ public class CategoryService {
 
     public Category createCategory(Category category) {
         return categoryRepository.save(category);
+    }
+
+    public List<Category> getAllCategory() {
+        return (List<Category>) categoryRepository.findAll();
+    }
+
+    public Category getCategoryById(int id) {
+        return categoryRepository.findById(id).orElse(null);
     }
 }
