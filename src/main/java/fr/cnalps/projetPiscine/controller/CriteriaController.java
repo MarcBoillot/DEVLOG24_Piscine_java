f392200baa761c85efb5090fbb80f387cc09a286
C-luigi <luigi.chiappa@le-campus-numerique.fr>
2023-09-27 10:05:34 +0200

    javadoc of Criteria #41

--- src/main/java/fr/cnalps/projetPiscine/controller/CriteriaController.java
+++ src/main/java/fr/cnalps/projetPiscine/controller/CriteriaController.java
@@ -7,6 +7,10 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+/**
+ * CriteriaController manages the web requests related to Criteria entities.
+ * Provides endpoints for retrieving all Criteria and getting a specific Criteria by ID.
+ */
 @RestController
 @RequestMapping(path="/criteria")
 public class CriteriaController {
@@ -14,30 +18,64 @@ public class CriteriaController {
     @Autowired
     private final CriteriaService service;
 
+    /**
+     * Constructor for CriteriaController.
+     *
+     * @param service The CriteriaService to handle business logic related to Criteria entities.
+     */
     public CriteriaController(CriteriaService service) {
         this.service = service;
     }
 
+    /**
+     * Creates a new Criteria object.
+     *
+     * @param criteria The Criteria object to be created.
+     * @return The created Criteria object.
+     */
     @PostMapping
     public Criteria createCriteria(@RequestBody Criteria criteria) {
         return service.createCriteria(criteria);
     }
 
+    /**
+     * Retrieves all Criteria object from the database.
+     *
+     * @return List of all Criteria object.
+     */
     @GetMapping
     public List<Criteria> getAllCriteria(){
         return this.service.getAllCriteria();
     }
 
+    /**
+     * Retrieves a specific Criteria by its ID.
+     *
+     * @param id The ID of the Criteria object.
+     * @return The Criteria entity matching the provided ID.
+     */
     @GetMapping("/{id}")
     public Criteria getCriteriaById (@PathVariable(name = "id") int id) {
         return this.service.getCriteriaById(id);
     }
 
+    /**
+     * Updates a specific Criteria object by its ID.
+     *
+     * @param id The ID of the Criteria object.
+     * @param criteria The updated Criteria object.
+     * @return The updated Criteria object.
+     */
     @PutMapping("/{id}")
     public Criteria updateCriteria(@PathVariable int id, @RequestBody Criteria criteria) {
         return service.updateCriteria(id, criteria);
     }
 
+    /**
+     * Deletes a specific Criteria object by its ID.
+     *
+     * @param id The ID of the Criteria object.
+     */
     @DeleteMapping("/{id}")
     public void deleteCriteria(@PathVariable int id) {
         service.deleteCriteria(id);
--- src/main/java/fr/cnalps/projetPiscine/service/CriteriaService.java
+++ src/main/java/fr/cnalps/projetPiscine/service/CriteriaService.java
@@ -8,28 +8,61 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+/**
+ * Service class for managing Criteria object.
+ * Provides methods for CRUD operations on Criteria object.
+ */
 @Service
 public class CriteriaService {
 
     private final CriteriaRepository criteriaRepository;
 
+    /**
+     * Constructor for CriteriaService.
+     *
+     * @param criteriaRepository The repository for accessing Criteria entities from the database.
+     */
     @Autowired
     public CriteriaService(CriteriaRepository criteriaRepository) {
         this.criteriaRepository = criteriaRepository;
     }
 
+    /**
+     * Creates and persists a new Criteria object.
+     *
+     * @param criteria The Criteria object to be created.
+     * @return The created Criteria object.
+     */
     public Criteria createCriteria (Criteria criteria){
         return criteriaRepository.save(criteria);
     }
 
+    /**
+     * Retrieves all Criteria object from the database.
+     *
+     * @return A list of all Criteria object.
+     */
     public List<Criteria> getAllCriteria() {
         return (List<Criteria>) criteriaRepository.findAll();
     }
 
+    /**
+     * Retrieves a specific Criteria object by its ID.
+     *
+     * @param id The ID of the desired Criteria.
+     * @return The Criteria object with the given ID or null if not found.
+     */
     public Criteria getCriteriaById(int id) {
         return criteriaRepository.findById(id).orElse(null);
     }
 
+    /**
+     * Updates a specific Criteria object.
+     *
+     * @param id The ID of the Criteria to be updated.
+     * @param criteria The updated Criteria object.
+     * @return The updated Criteria object or null if not found.
+     */
     public Criteria updateCriteria(int id, Criteria criteria) {
         if (criteriaRepository.existsById(id)) {
             criteria.setId(id);
@@ -38,6 +71,11 @@ public class CriteriaService {
         return null;
     }
 
+    /**
+     * Deletes a specific Criteria object by its ID.
+     *
+     * @param id The ID of the Criteria to be deleted.
+     */
     public void deleteCriteria(int id) {
         if (criteriaRepository.existsById(id)) {
             criteriaRepository.deleteById(id);
