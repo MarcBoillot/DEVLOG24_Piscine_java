ed0a085471759549c8d1149211f9a48cbb8ab1ba
C-luigi <luigi.chiappa@le-campus-numerique.fr>
2023-09-28 11:45:24 +0200

    controller group #56

---
+++ src/main/java/fr/cnalps/projetPiscine/controller/GroupCandidateController.java
@@ -0,0 +1,20 @@
+package fr.cnalps.projetPiscine.controller;
+
+import fr.cnalps.projetPiscine.model.GroupCandidate;
+import fr.cnalps.projetPiscine.service.GroupCandidateService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path="/group")
+public class GroupCandidateController {
+
+    @Autowired
+    private final GroupCandidateService service;
+
+    public GroupCandidateController(GroupCandidateService service) {
+        this.service = service;
+    }
+}
